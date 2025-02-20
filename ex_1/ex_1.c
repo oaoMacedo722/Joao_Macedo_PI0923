e69c81c568ebe8cf54ee5134b8dbb0cf8d1aa88d
Joao Macedo <joao.macedo.t0126442@edu.atec.pt>
2025-02-20 09:56:51 +0000

    elaboração do ex1

--- ex_1/ex_1.c
+++ ex_1/ex_1.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main (){
+int Num=0;
+for(int i=0;i<10;i++){
+    Num+=2;
+    printf("Num = %d,i= %d",Num,i);
+    printf("aquiFor");
 
-printf("asdasd");
+}
 return 0;
-
 }
