563e6df6aa7fb809ff40a7a5d4eeb41e866b2905
Joao Macedo <Joao.macedo.t0126442@edu.atec.pt>
2025-02-25 13:25:20 +0000

    ex 4 concluido

---
+++ ex_4/Untitled1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int saldo;
+    int cheque;
+    int sc;
+
+    printf("Insira o seu saldo: ");
+    scanf("%d", &saldo);
+    printf("Digite o valor do cheque: ");
+    scanf("%d", &cheque);
+
+if(saldo>cheque){
+       saldo-cheque = sc ;
+    printf("%d",sc);
+}
+else{
+    printf("O valor nao pode ser descontado");
+}
+
+
+
+
+}
